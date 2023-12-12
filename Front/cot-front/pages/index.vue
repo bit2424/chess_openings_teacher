93f5ff8fcee2101d99601939e5a8ee883a0c1887
Nelson David Quiñones Virgen <44851531+bit2424@users.noreply.github.com>
2023-12-12 17:44:43 -0500

    :construction: Working on the projects landing page

--- Front/cot-front/pages/index.vue
+++ Front/cot-front/pages/index.vue
@@ -3,19 +3,60 @@ const route = useRoute()
 </script>
 
 <template>
-  <div class="main-container">
-    <h1>Nuxt Routing set up successfully!</h1>
-    <p>Current route: {{ route.path }}</p>
-    <a href="https://nuxt.com/docs/getting-started/routing" target="_blank">Learn more about Nuxt Routing</a>
+  <div class="landing-page">
+
+    
+    <main>
+      <div>
+        <img src="your-image.png" alt="Project hero image" />
+        <h1>Mini Chess Engine: Play & Learn with AI</h1>
+        <p>Practice your openings, explore LLM technology, and hone your Vue.js skills.</p>
+      </div>
+      <h2>Features & Benefits</h2>
+      <ul>
+        <li>Play against a fully functional chess engine.</li>
+        <li>Get move recommendations from a powerful language model.</li>
+        <li>Learn from your mistakes and improve your opening strategy.</li>
+        <li>Experience the power of AI in action.</li>
+      </ul>
+
+      <h2>How it Works</h2>
+      <p>Simply start playing, and the LLM will analyze the board and suggest moves based on its knowledge and your playing style.</p>
+      <ul>
+        <li>Click on a piece to move it.</li>
+        <li>The LLM will highlight its recommended move.</li>
+        <li>You can choose to follow the recommendation or play your own move.</li>
+      </ul>
+
+      <h2>Your Learning Journey</h2>
+      <p>This project is not just about playing chess, it's about learning and growing.</p>
+      <ul>
+        <li>Experiment with different strategies and see how the LLM reacts.</li>
+        <li>Analyze your games and learn from your mistakes.</li>
+        <li>Discover how AI can enhance your chess skills.</li>
+      </ul>
+
+      <h2>Start Playing Now!</h2>
+      <!-- <button @click="playGame">Play Chess & Learn with AI</button> -->
+
+      <a href="/learn-more">Learn More About the LLM Technology</a>
+      <a href="/my-journey">Follow My AI & Vue.js Learning Journey</a>
+    </main>
+
+    <footer>
+      <p>&copy; Your Name 2023</p>
+    </footer>
   </div>
 </template>
 
+
+
 <style>
-  .center-container {
-    display: flex;
+  .landing-page{
+    /* display: flex; */
     justify-content: center;
-    align-items: center;
-    /* height: 100vh; Set the container to 100% of the viewport height */
-    width:100vw;
+    width:96w;
+    height:100vh;
+    margin:3vw;
   }
 </style>
